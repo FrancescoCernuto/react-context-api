61ead8fab658a8c435c84f0abf22ec644951b05e
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-13 16:35:19 +0100

    PostContext function + prop children

--- src/assets/contexts/PostContext.jsx
+++ src/assets/contexts/PostContext.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import { createContext, useContext } from "react";
 
+//  Consumer
 const PostContext = createContext();
 export const usePostContext = () => useContext(PostContext);
+
+// Provider
+export function PostContextProvider({ children })

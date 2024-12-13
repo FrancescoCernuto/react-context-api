f79c69b7fc8c943f10be5cc5ff56ae8b5a760747
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-13 16:28:06 +0100

    export context

--- src/assets/contexts/PostContext.jsx
+++ src/assets/contexts/PostContext.jsx
@@ -1,2 +1,5 @@
 import { useEffect, useState } from "react";
 import { createContext, useContext } from "react";
+
+const PostContext = createContext();
+export const usePostContext = () => useContext(PostContext);

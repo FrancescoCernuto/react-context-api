83c9858e9ac9a6dd6d75292b226c0117f3a910c3
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-13 16:04:04 +0100

    import bootstrap/assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
 import { PostContextProvider } from "./assets/contexts/PostContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import PostsPage from "./assets/pages/PostsPage";

742690f8e1c811c98543cd7d6499aedf6aad360e
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-13 17:46:03 +0100

    wrap app

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,17 @@ import { PostContextProvider } from "./assets/contexts/PostContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import PostsPage from "./assets/pages/PostsPage";
+
+export default function App() {
+  return (
+    <PostContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index Component={PostsPage} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </PostContextProvider>
+  );
+}
